c931ef994a82ee0c946e11e7920f880b723ab954
AB7zz <abhinavcv007@gmail.com>
2024-04-06 02:54:48 +0530

    map works fine

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
+} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
-    const { mapid } = useParams()
-  return (
-    <div>{mapid}</div>
+  const { mapid } = useParams()
+  const position = [51.505, -0.09]
+      
+  return(
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
